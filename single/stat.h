c20fe846c38ba44930eab07db23a01048e1b360d
DerThorsten <derthorstenbeier@gmail.com>
2025-02-07 10:58:38 +0100

    disabled parts of the code

--- single/stat.h
+++ single/stat.h
@@ -8,10 +8,11 @@ c
      c     treorthu, treorthv, telru, telrv, tbsvd, tnorm2,
      c     tlansvd, tritzvec, trestart, treorth, tdot
 
-
+#ifdef ENABLE_COMMON_TIMING
       common/timing/ nopx, nreorth, ndot, nreorthu, nreorthv, nitref,
      c     nrestart, nbsvd, tmvopx, tgetu0, tupdmu, tupdnu, tintv,
      c     tlanbpro, treorth, treorthu, treorthv, telru, telrv, tbsvd,
      c     tnorm2, tlansvd, nlandim, tritzvec, trestart, tdot, nsing
 
       external second
+#endif
