573024298f68bf66a1d82a3caeee9acd6c52dc60
DerThorsten <derthorstenbeier@gmail.com>
2025-02-07 11:20:16 +0100

    use thorstens propack fork II

--- complex8/stat.h
+++ complex8/stat.h
@@ -7,11 +7,3 @@ c
       real    tmvopx, tgetu0, tupdmu, tupdnu, tintv, tlanbpro,
      c     treorthu, treorthv, telru, telrv, tbsvd, tnorm2,
      c     tlansvd, tritzvec, trestart, treorth, tdot
-
-
-c      common/timing/ nopx, nreorth, ndot, nreorthu, nreorthv, nitref,
-c     c     nrestart, nbsvd, tmvopx, tgetu0, tupdmu, tupdnu, tintv,
-c     c     tlanbpro, treorth, treorthu, treorthv, telru, telrv, tbsvd,
-c     c     tnorm2, tlansvd, nlandim, tritzvec, trestart, tdot, nsingc
-c
-c      external second
--- single/stat.h
+++ single/stat.h
@@ -7,12 +7,3 @@ c
       real    tmvopx, tgetu0, tupdmu, tupdnu, tintv, tlanbpro,
      c     treorthu, treorthv, telru, telrv, tbsvd, tnorm2,
      c     tlansvd, tritzvec, trestart, treorth, tdot
-
-#ifdef ENABLE_COMMON_TIMING
-      common/timing/ nopx, nreorth, ndot, nreorthu, nreorthv, nitref,
-     c     nrestart, nbsvd, tmvopx, tgetu0, tupdmu, tupdnu, tintv,
-     c     tlanbpro, treorth, treorthu, treorthv, telru, telrv, tbsvd,
-     c     tnorm2, tlansvd, nlandim, tritzvec, trestart, tdot, nsing
-
-      external second
-#endif
